bf8f4944c7df8c5574a396f842dd67b580918e48
Jeff Butler <jeffgbutler@gmail.com>
2020-05-06 14:52:18 -0400

    Switch to property based datasource configuration

--- src/main/java/com/example/cockroachdemo/MyBatisConfiguration.java
+++ src/main/java/com/example/cockroachdemo/MyBatisConfiguration.java
@@ -8,38 +8,33 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+/**
+ * This class configures MyBatis and sets up mappers for injection.
+ * 
+ * When using the Spring Boot Starter, using a class like this is completely optional unless you need to
+ * have some mappers use the BATCH executor (as we do in this demo). If you don't have that requirement,
+ * then you can remove this class. By Default, the MyBatis Spring Boot Starter will find all mappers
+ * annotated with @Mapper and will automatically wire your Datasource to the underlying MyBatis
+ * infrastructure.
+ */
 @Configuration
 @MapperScan(basePackages = "com.example.cockroachdemo.mapper", annotationClass = Mapper.class)
 @MapperScan(basePackages = "com.example.cockroachdemo.batchmapper", annotationClass = Mapper.class,
             sqlSessionTemplateRef = "batchSqlSessionTemplate")
-/**
- * This class configures MyBatis and sets up mappers for injection.
- * 
- * Most of the elements in this class are optional. This class demonstrates how to configure MyBatis when
- * some mappers require a BATCH executor. If you don't have that requirements, then you can remove everything from
- * this class except possibly the Datasource configuration. By Default, the MyBatis Spring Boot Starter will find all
- * mappers annotated with @Mapper and will automatically wire your Datasource to the underlying MyBatis infrastructure.
- */
 public class MyBatisConfiguration {
 
-    @Bean
-    public DataSource datasource() {
-        return DataSourceBuilder.create()
-            .driverClassName("org.postgresql.Driver")
-            .url("jdbc:postgresql://localhost:26257/bank?ssl=false")
-            .username("maxroach")
-            .build();
-    }
+    @Autowired
+    private DataSource dataSource;
 
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
-        factory.setDataSource(datasource());
+        factory.setDataSource(dataSource);
         return factory.getObject();
     }
 
